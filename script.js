0e931337f9706722e18ef3bec2c650f692399fc1
lilmorpheus <Escott97@hotmail.co.uk>
2022-06-01 14:29:09 +0100

    Basci Visuals

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+var num_input = document.getElementById('num_input');
+
+var isPrime = document.getElementById('isPrime')
+var notPrime = document.getElementById('notPrime')
+
+reset();
+
+num_input.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        reset();
+        var num = isNumber(num_input.value);
+        if (num) {
+            isThisPrime(num);
+        }
+    }
 })
 
-readline.question('Enter Your Prime Number> ', number => {
-    console.log(`Number ${number}`)
+function isNumber(number) {
+    /* Verfication */
+    var val = parseInt(number);
+    if (!val) {
+        alert("Numbers are Required")
+        return null;
+    }
+    if (number % 1 != 0 ) {
+        alert("Whole Integers Required")
+        return null
+    }
+    return val;
+}
 
+function isThisPrime(number) { 
     /* START */
     var factors = [];
     var num = parseInt(number)
@@ -15,9 +39,18 @@ readline.question('Enter Your Prime Number> ', number => {
 
     if(factors.length > 0) {
         console.log(factors)
+        console.log(factors.join())
+        document.getElementById('factors').innerHTML = factors.join(', ');
+        console.log(document.getElementById('factors').innerHTML)
+        notPrime.style.display = 'block';
     } else {
         console.log('\n PRIME')
+        isPrime.style.display = 'block';
     }
     /* END */
-    readline.close();
-})
+}
+
+function reset() {
+    notPrime.style.display = 'none';
+    isPrime.style.display = 'none';
+}
